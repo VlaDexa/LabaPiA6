9b4e6de641125193ba075054d97a3c0f4e79b6da
Владислав Гречанник <m2108502@edu.misis.ru>
2021-12-15 12:21:43 +0300

    Second lesson

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
+using System;
 using static System.Console;
 
 namespace LabaPiA6
@@ -10,14 +10,16 @@ namespace LabaPiA6
         {
             FirstOne();
             WriteLine();
+            FirstTwo();
+            WriteLine();
         }
 
-            struct Contestant
+        struct Contestant
         {
             private readonly string surname;
             private readonly string community;
-            public readonly double length_1;
-            public readonly double length_2;
+            private readonly double length_1;
+            private readonly double length_2;
 
             public Contestant(string surname, string community, double length_1, double length_2)
             {
@@ -27,23 +29,90 @@ namespace LabaPiA6
                 this.length_2 = length_2;
             }
 
+            public double GetLength()
+            {
+                return length_1 + length_2;
+            }
+
             public override string ToString()
             {
-                return string.Format("{0} | {1} | {2} | {3}", surname, community, length_1, length_2) ;
+                return string.Format("{0}\t|{1}\t\t|{2}\t\t|{3}", surname, community, length_1, length_2);
             }
         }
 
         static private void FirstOne()
         {
             Contestant[] contestants = {
-                new Contestant("Белов", "Легница", 2.4, 2.8),
+                new Contestant("Дубова", "Клуж", 2.1, 1.5),
                 new Contestant("Цветков", "Ичан", 2.2, 2.2),
-                new Contestant("Дубова", "Клуж", 2.1, 1.5)
+                new Contestant("Белов", "Рат", 2.4, 2.8),
             };
-            contestants.OrderBy((el) => el.length_1 + el.length_2);
-            WriteLine("Фамилия | Общество | Первый прыжок | Второй прыжок");
-            foreach (var contestant in contestants)
+            WriteLine("Фамилия\t|Общество\t|Первый прыжок\t|Второй прыжок");
+            foreach (var contestant in contestants.OrderByDescending((el) => el.GetLength()))
                 WriteLine("{0}", contestant);
+        }
+
+        struct SwimContestant
+        {
+            private readonly string name;
+            private readonly string group;
+            private readonly string teacher;
+            private readonly double result;
+
+            public SwimContestant(string name, string group, string teacher, double result)
+            {
+                this.name = name;
+                this.group = group;
+                this.teacher = teacher;
+                this.result = result;
+            }
+
+            public bool IsGood()
+            {
+                return result < 1.45;
+            }
+
+            public override string ToString()
+            {
+                return $"{name}\t|{group}\t|{teacher}\t|{result}\t\t|{(IsGood() ? "Зачёт" : "Незачёт")}";
+            }
+        }
+
+        static private void FirstTwo()
+        {
+            SwimContestant[] swimContestants = { 
+                new SwimContestant("Ксения", "Шанцю", "Орехова", 1.51),
+                new SwimContestant("София", "Ашдод", "Никонов", 1.33),
+                new SwimContestant("Нина", "Йена", "Захаров", 1.61),
+            };
+            WriteLine("Имя\t|Группа\t|Тренер\t\t|Результат\t|Зачёт");
+            foreach (var swimContestant in swimContestants)
+                WriteLine($"{swimContestant}");
+            WriteLine($"{swimContestants.Where(el => el.IsGood()).Count()} человек получил зачёт");
+        }
+
+        struct Answer
+        {
+            private readonly string text;
+            public readonly double Percentage { get; }
+
+            public Answer(string text, double percentage)
+            {
+                this.text = text;
+                Percentage = percentage;
+            }
+
+            public override string ToString()
+            {
+                return $"{text}\t|{Percentage}";
+            }
+        }
+
+        static private void FirstThree()
+        {
+            Answer[] answers = {
+                new Answer();
+            };
         }
     }
 }
